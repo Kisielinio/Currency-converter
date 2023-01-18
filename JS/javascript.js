331618ad5b631725d25f7886fe665b0a8c8b83eb
Michal Motowidlo <motowidlo94@gmail.com>
2023-01-18 21:50:39 +0100

    Initial commit

---
+++ JS/javascript.js
@@ -0,0 +1,34 @@
+let formElement = document.querySelector(".js-form");
+let valueElement = document.querySelector(".js-value");
+let currencyElement = document.querySelector(".js-currency");
+let resultElement = document.querySelector(".js-result");
+let CHF = 4.74;
+let EUR = 4.69;
+let GBP = 5.35;
+let USD = 4.71;
+
+
+formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let value = valueElement.value;
+    let currency = currencyElement.value;
+    let result;
+Funckja 
+    switch (currency) {
+        case "CHF":
+            result = value / CHF;
+            break;
+        case "EUR":
+            result = value / EUR;
+            break;
+        case "GBP":
+            result = value / GBP;
+            break;
+        case "USD":
+            result = value / USD;
+    }
+    resultElement.innerText = `${result.toFixed(2)} ${currency}`;
+});
+
+
